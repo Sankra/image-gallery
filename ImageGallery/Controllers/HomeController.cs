b5c8dcd63c6456fbe06e6263da1be6ab729f984f
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2019-04-19 23:05:21 +0200

    Name and logo is configurable

--- ImageGallery/Controllers/HomeController.cs
+++ ImageGallery/Controllers/HomeController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ImageGallery.Configuration;
 using ImageGallery.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,19 +15,21 @@ namespace ImageGallery.Controllers
     public class HomeController : Controller
     {
         readonly IAlbumService albumService;
+        readonly IConfiguration configuration;
 
-        public HomeController(IAlbumService albumService)
+        public HomeController(IAlbumService albumService, IConfiguration configuration)
         {
             // TODO: Favicon
             // TODO: Gråe ut alle klikkbare menypunkter når  musen er over dem
             // TODO: Gjør noe smartere med paddingen jeg slenger rundt med i alle views
             this.albumService = albumService;
+            this.configuration = configuration;
         }
 
         [ResponseCache(NoStore = true)]
         public async Task<IActionResult> Index()
         {
-            ViewData["Title"] = "DIPS Photos";
+            ViewData["Title"] = configuration["Customization:SiteName"];
             ViewData["ShowAdd"] = true;
             var albumPreviews = await albumService.GetAlbumPreviews();
             return View(albumPreviews);
--- ImageGallery/Startup.cs
+++ ImageGallery/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 
 namespace ImageGallery
 {
@@ -33,6 +34,7 @@ namespace ImageGallery
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             Enum.TryParse(Configuration["BackingStore"], out BackingStore backingStore);
+            // TODO: Change Exception text when another store is added
             switch (backingStore)
             {
                 case BackingStore.File:
@@ -41,6 +43,10 @@ namespace ImageGallery
                 default:
                     throw new ArgumentException("BackingStore must be either file or TODO");
             }
+
+            //var a = Configuration["Customization"];
+            //var customization = JsonConvert.DeserializeObject<Customization>(a);
+            //services.AddSingleton(customization);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
