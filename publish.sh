ca927bdede76ec7dbaaa041773d06940e32830c7
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2019-04-24 21:18:17 +0200

    Added publish scripts

--- publish.sh
+++ publish.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 set -e
-
-VERSION=$(cat MediaServer/wwwroot/VERSION.txt)
-
 ./build.sh
-docker tag dips/media-server:latest vt-optimus-solr02:5000/dips/media-server:$VERSION
-docker push vt-optimus-solr02:5000/dips/media-server:$VERSION
+# az login
+az acr login --name hjerpbakk
+docker tag hjerpbakk/image-gallery:latest hjerpbakk.azurecr.io/hjerpbakk/image-gallery
+docker push hjerpbakk.azurecr.io/hjerpbakk/image-gallery
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 set -e
+./build.sh
 docker run -p 80:80 hjerpbakk/image-gallery
